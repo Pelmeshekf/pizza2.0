69690d8134b09db9c6d63aa3830b3c8b21f170c0
Serhii <@mr.pelmenbchik@gmail.com>
2024-04-20 14:28:18 +0300

    Добавлен функционал

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from './Header'
+import Sort from './Sort'
+import Categories from './Categories'
+import PizzaBlock from './PizzaBlock'
+
+export { Header, Sort, Categories, PizzaBlock }
